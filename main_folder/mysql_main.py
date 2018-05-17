238dae652010aa17058ce983cf280cba82c69eff
xerezlolz <kobehjy24@126.com>
2018-05-17 22:39:20 +0800

    数据库连接

--- main_folder/mysql_main.py
+++ main_folder/mysql_main.py
@@ -4,9 +4,10 @@
 import pymysql
 
 # 连接数据库
-def load_mysqldb():
-    print("连接到服务器...")
-    com = pymysql.connect(host='localhost', port=3306, user='root', password='modi123456', db='WORKDB')
-    print("连接成功")
-    return com
+print("连接到服务器...")
+com = pymysql.connect(host='localhost', port=3306, user='root', password='modi123456', 
+db='work db', charset='utf-8')
+
+# 获取游标
+cursor = com.cursor()
 
