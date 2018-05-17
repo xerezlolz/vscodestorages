9d8189df369d0861a930c880a163018fac68f357
xerezlolz <kobehjy24@126.com>
2018-05-17 21:43:50 +0800

    连接数据库

--- main_folder/mysql_main.py
+++ main_folder/mysql_main.py
@@ -6,8 +6,7 @@ import pymysql
 # 连接数据库
 def load_mysqldb():
     print("连接到服务器...")
-    com = pymysql.connect("localhost", "root", "modi123456", "FORMUALDB")
+    com = pymysql.connect(host='localhost', port=3306, user='root', password='modi123456', db='WORKDB')
     print("连接成功")
     return com
-
 
