ce48af5f3c9414a24b7304bba59d12c10f0f3b73
xerezlolz <kobehjy24@126.com>
2018-05-18 10:55:23 +0800

    总标释

--- main_folder/mysql_main.py
+++ main_folder/mysql_main.py
@@ -3,10 +3,14 @@
 
 import pymysql
 
+'''
+连接数据库块'workspace_db,通过游标fech数据
+
+'''
 # 连接数据库
 print("连接到服务器...")
-com = pymysql.connect(host='localhost', port=3306, user='root', password='modi123456', 
-db='work db', charset='utf-8')
+com = pymysql.connect(host='localhost', port=3306, user='root', passwd='modi123456', 
+db='work db', charset='utf8')
 
 # 获取游标
 cursor = com.cursor()
